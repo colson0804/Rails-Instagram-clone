eaed25cac596ef8733ffa907ccdc3ec8465b2fe6
colson0804 <craigolson2015@u.northwestern.edu>
2015-07-31 17:02:38 -0500

    Log in and Log out

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -10,12 +10,22 @@ feature 'User authentication' do
     expect(page).to_not have_content('New Post')
 
     click_link 'Login'
-    fill_in 'Email', with: 'example@gmail.com'
+    fill_in 'Email', with: 'sample@gmail.com'
     fill_in 'Password', with: 'foobar123'
-    click_button('Log In')
+    click_button('Log in')
 
     expect(page).to have_content('Signed in successfully.')
     expect(page).to_not have_content('Register')
     expect(page).to have_content('Logout')
+  end
+
+  scenario 'can log out once logged in' do
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: 'sample@gmail.com'
+    fill_in 'Password', with: 'foobar123'
+    click_button 'Log in'
+    click_link 'Logout'
+    expect(page).to have_content 'Signed out successfully.'
   end
 end  
