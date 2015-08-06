fdcbba26e5c9bcff541af2f908cebd20ef820987
colson0804 <craigolson2015@u.northwestern.edu>
2015-08-05 22:12:40 -0500

    added delete comment test

---
+++ spec/features/deleting_comment_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper.rb'
+
+feature 'Deleting posts' do  
+  background do
+    user_one = create(:user)
+    user_two = create(:user, id: 2, user_name: "user2", email: "user2@gmail.com")
+    post = create(:post, user_id: 1)
+    comment = create(:comment, user_id: 2, post_id: post.id)
+    sign_in_with user_two
+  end
+  scenario 'can delete a post' do
+    visit '/'
+    expect(page).to have_content('MyString')
+    click_link("delete-1")
+    expect(page).to have_content('Comment deleted :(')
+  end
+end 
